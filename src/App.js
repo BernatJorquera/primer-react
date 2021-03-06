aaa6ac3e29a967f8a3d8baf9a13dcb11461cbba3
Bernat Jorquera <bernatjv19@gmail.com>
2021-03-06 22:42:19 +0100

    Favicon, título y texto

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <p>Texto</p>
+    <p>Me he pasado horas buscando el meme perfecto para poner aquí y cuando lo he encontrado he visto que me da algun error. Pero no me ha dado mucha rabia porque en realidad no había encontrado el meme decente, solo había encontrado uno decente.</p>
   );
 }
 
